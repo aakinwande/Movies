0dc33068e84a24bad433cc09f2c4a0a656936d97
akinwande <wandeyameen@gmail.com>
2023-02-15 15:44:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import MovieCard from "./components/MovieCard";
+import YouTube from "react-youtube";
 
 function App() {
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280";
+
+  const API_URL = "https://api.themoviedb.org/3";
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  const [selectedMovie, setSelectedMovie] = useState({});
+  const [playTrailer, setPlayTrailer] = useState(false);
+
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover";
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}/${type}/movie`, {
+      params: {
+        api_key: process.env.REACT_APP_MOVIES_API_KEY,
+        query: searchKey,
+      },
+    });
+    setMovies(results);
+    await selectMovieVid(results[0]);
+  };
+
+  const fetchMovieVid = async (id) => {
+    const { data } = await axios.get(`${API_URL}/movie/${id}`, {
+      params: {
+        api_key: process.env.REACT_APP_MOVIES_API_KEY,
+        append_to_response: "videos",
+      },
+    });
+    return data;
+  };
+
+  const selectMovieVid = async (movie) => {
+    setPlayTrailer(false);
+    const data = await fetchMovieVid(movie.id);
+    setSelectedMovie(data);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const renderMovies = () =>
+    movies.map((movie) => (
+      <MovieCard key={movie.id} movie={movie} selectMovie={selectMovieVid} />
+    ));
+
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchKey);
+  };
+
+  const renderTrailer = () => {
+    const trailer = selectedMovie.videos.results.find(
+      (vid) => vid.name === "Official Trailer"
+    );
+    const key = trailer ? trailer.key : selectedMovie.videos.results[0].key;
+
+    return (
+      <YouTube
+        videoId={key}
+        // containerClassName={"youtube-container"}
+        // opts={{
+        //   width: "100%",
+        //   height: "100%",
+        //   playerVars: {
+        //     autoplay: 1,
+        //     controls: 0,
+        //   },
+        // }}
+      />
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className={"header"}>
+        <div className={"header-content max-center"}>
+          <span>Movie Trailer App</span>
+
+          <form onSubmit={searchMovies}>
+            <input type="text" onChange={(e) => setSearchKey(e.target.value)} />
+            <button type={"submit"}>Search</button>
+          </form>
+        </div>
       </header>
+
+      <div
+        className="hero"
+        style={{
+          backgroundImage: `url('${IMAGE_PATH}${selectedMovie.backdrop_path}')`,
+        }}
+      >
+        <div className="hero-content max-center">
+          {playTrailer ? 
+            <button
+              className={"button button--close"}
+              onClick={() => setPlayTrailer(false)}
+            >
+              Close
+            </button>
+           : null}
+          {selectedMovie.videos && playTrailer ? renderTrailer() : null}
+          <button className={"button"} onClick={() => setPlayTrailer(true)}>
+            Play Trailer
+          </button>
+          <h1 className={"hero-title"}>{selectedMovie.title}</h1>
+          {selectedMovie.overview ? (
+            <p className={"hero-overview"}>{selectedMovie.overview}</p>
+          ) : null}
+        </div>
+      </div>
+
+      <div className="container max-center">{renderMovies()}</div>
     </div>
   );
 }
